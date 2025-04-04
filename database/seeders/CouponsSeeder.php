7f48a723309776545561f8dd1df69a7f0b1fbedf
1Lucas-Matheus <lucasmsilvam01@gmail.com>
2025-04-04 16:50:35 -0300

    Seed Coupons

--- database/seeders/CouponsSeeder.php
+++ database/seeders/CouponsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CouponsSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class CouponsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('products')->insert([
+            ['value' => 10, 'key' => 1],
+            ['value' => 20, 'key' => 1],
+            ['value' => 30, 'key' => 1],
+            ['value' => 40, 'key' => 1],
+            ['value' => 50, 'key' => 1]
+        ]);
     }
 }
