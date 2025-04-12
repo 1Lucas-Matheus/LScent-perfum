9bb77aee0d0f2e79b610791d5c6a6cf059861f07
1Lucas-Matheus <lucasmsilvam01@gmail.com>
2025-04-12 19:09:26 -0300

    Remocao da welcome

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
