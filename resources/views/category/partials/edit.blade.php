62a7fa12e21e8720ebbb801f351fda2df5b4514a
1Lucas-Matheus <lucasmsilvam01@gmail.com>
2025-04-12 21:04:48 -0300

    Design edit categories

--- resources/views/category/partials/edit.blade.php
+++ resources/views/category/partials/edit.blade.php
@@ -8,19 +8,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atualizar categoria</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    @include('layouts.sidebar')
     @include('components.messageAlert')
 </head>
 
-<body class="bg-white flex min-h-screen">
-    <div class="flex justify-center items-center w-full h-screen">
-        <div class="bg-white w-[400px] flex flex-col rounded-lg px-10 shadow-[10px_10px_30px_rgba(0,0,0,0.2)] items-center justify-center">
-            <h1 class="text-gray-900 my-6 text-4xl font-semibold text-center">Atualizar categoria</h1>
+<body class="flex min-h-screen bg-white">
 
-            <div class="messageAlert">
-                    @yield('messageAlert')
-                </div>
+    <div class="sidebar">
+        @yield('sidebar')
+    </div>
 
-            <form action="{{ route('categories.update', ['category' => $category->id]) }}" method="post" class="w-full flex flex-col">
+    <div class="flex items-center justify-center w-full min-h-screen p-4 sm:ml-64 bg-gray-50">
+        <div class="bg-white w-full max-w-md flex flex-col rounded-lg px-10 py-8 shadow-[10px_10px_30px_rgba(0,0,0,0.1)]">
+            <h1 class="mb-6 text-3xl font-semibold text-center text-gray-900">Novo lembrete</h1>
+
+            <div class="mb-4 messageAlert">
+                @yield('messageAlert')
+            </div>
+
+            <form action="{{ route('categories.update', ['category' => $category->id]) }}" method="post" class="flex flex-col w-full">
                 @csrf
 
                 <input type="hidden" name="_method" value="put">
@@ -28,14 +34,13 @@
                 <input type="text" id="input-group-1" class="bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-100 dark:border-gray-400 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ $category->name }}" name="name" required>
 
                 <div class="flex justify-end gap-3 mt-4 mb-3">
-                    <a href="{{ route('categories.index') }}"
-                        class="bg-gray-700 hover:bg-red-800 px-5 py-2 rounded-lg text-white font-medium transition duration-300">
-                        Cancelar
+                    <a href="{{ route('categories.index') }}" class="px-5 py-2 font-medium text-white transition duration-300 bg-gray-700 rounded-lg hover:bg-red-800"> Cancelar
                     </a>
                     <input type="submit" value="Atualizar" class="bg-[#19231F] hover:bg-green-900 px-6 py-2 rounded-lg text-white font-medium transition duration-300 cursor-pointer">
                 </div>
             </form>
         </div>
+    </div>
 </body>
 
 </html>
